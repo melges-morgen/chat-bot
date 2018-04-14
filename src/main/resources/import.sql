111cba735fd9e7c91e8edf1b626de2c711b996b6
Morgen Matvey <matvey.morgen@revolut.com>
2018-04-14 07:43:38 +0300

    Fix in import.sql update dialect version.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,9 @@
-INSERT INTO attributes (id, attribute_class, attribute_description, attribute_name) VALUES (unhex("b068931cc450342ba3f5b3d276ea4297"), "ru.frtk.das.microtypes.StringValue", "Имя", "name");
-INSERT INTO attributes (id, attribute_class, attribute_description, attribute_name) VALUES (unhex("efd6db0740fd3b4094e6a1d339fa2046"), "ru.frtk.das.microtypes.StringValue", "Имя в родительном падеже", "name_gen");
-INSERT INTO attributes (id, attribute_class, attribute_description, attribute_name) VALUES (unhex("e8911889a7783cdab79c3d6ae86834b2"), "ru.frtk.das.microtypes.StringValue", "Фамилия", "surname");
-INSERT INTO attributes (id, attribute_class, attribute_description, attribute_name) VALUES (unhex("e8911889a7783cdab79c3d6ae86834b2"), "ru.frtk.das.microtypes.StringValue", "Фамилияd в родительном падеже", "surname_gen");
-INSERT INTO attributes (id, attribute_class, attribute_description, attribute_name) VALUES (unhex("9cbcc716b7373899904aa09cdacee546"), "ru.frtk.das.microtypes.StringValue", "Дата рождения", "birth_date");
-INSERT INTO attributes (id, attribute_class, attribute_description, attribute_name) VALUES (unhex("139c8722e29638e2a23333670b074a05"), "ru.frtk.das.microtypes.StringValue", "Название шаблона", "template_name");
-INSERT INTO attributes (id, attribute_class, attribute_description, attribute_name) VALUES (unhex("8cb279960c683790afffe707588bb3a1"), "ru.frtk.das.microtypes.StringValue", "Мужской пол", "is_male");
-INSERT INTO attributes (id, attribute_class, attribute_description, attribute_name) VALUES (unhex("e7bdd2d75d1138d3a867ae55f84bce3f"), "ru.frtk.das.microtypes.StringValue", "Текущая дата", "date_today");
+INSERT INTO attributes (id, attribute_class, attribute_description, attribute_name) VALUES (unhex("b068931cc450342ba3f5b3d276ea4297"), "ru.frtk.das.microtypes.StringValue", "Name", "name");
+INSERT INTO attributes (id, attribute_class, attribute_description, attribute_name) VALUES (unhex("efd6db0740fd3b4094e6a1d339fa2046"), "ru.frtk.das.microtypes.StringValue", "Name Generative", "name_gen");
+INSERT INTO attributes (id, attribute_class, attribute_description, attribute_name) VALUES (unhex("e8911889a7783cdab79c3d6ae86834b2"), "ru.frtk.das.microtypes.StringValue", "Surname", "surname");
+INSERT INTO attributes (id, attribute_class, attribute_description, attribute_name) VALUES (unhex("fb0f6d4785c837c1a68b723617a71880"), "ru.frtk.das.microtypes.StringValue", "Surname generative", "surname_gen");
+INSERT INTO attributes (id, attribute_class, attribute_description, attribute_name) VALUES (unhex("9cbcc716b7373899904aa09cdacee546"), "ru.frtk.das.microtypes.StringValue", "Birth date", "birth_date");
+INSERT INTO attributes (id, attribute_class, attribute_description, attribute_name) VALUES (unhex("139c8722e29638e2a23333670b074a05"), "ru.frtk.das.microtypes.StringValue", "Template name", "template_name");
+INSERT INTO attributes (id, attribute_class, attribute_description, attribute_name) VALUES (unhex("8cb279960c683790afffe707588bb3a1"), "ru.frtk.das.microtypes.StringValue", "Is male?", "is_male");
+INSERT INTO attributes (id, attribute_class, attribute_description, attribute_name) VALUES (unhex("e7bdd2d75d1138d3a867ae55f84bce3f"), "ru.frtk.das.microtypes.StringValue", "Current date", "date_today");
 
